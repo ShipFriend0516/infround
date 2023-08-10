fbce3166c08d05a0e20bf60ac89d34c2713538c4
JeongwooSeo <98446924+ShipFriend0516@users.noreply.github.com>
2023-08-10 22:26:00 +0900

    사이드바 계획

---
+++ src/Components/SideBar.js
@@ -0,0 +1,14 @@
+import React, {useEffect, useRef, useState } from "react";
+import {} from 'react-icons'
+
+const SideBar = ({ width=280, children }) => {
+
+    return (
+        <>
+            <div className='navBr'></div>
+        </>
+    )
+}
+
+
+export default SideBar;
