677a88cc7d5c3a2b7a235b972d5214e8ce967fda
Oracionspace <shipfriend4371@naver.com>
2023-11-07 15:16:07 +0900

    커버 페이지 모바일 반응형 추가 및 지도 200m 반경 확인

--- src/Components/Cover.js
+++ src/Components/Cover.js
@@ -1,34 +1,41 @@
-import { useNavigate } from 'react-router-dom';
-import logoImage from '../img/infround-logo.jpg'
+import { useNavigate } from "react-router-dom";
+import logoImage from "../img/infround-logo.jpg";
 
 const Cover = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleStartButtonClick = () => {
-    navigate('/map')
+    navigate("/map");
   };
 
   return (
-    <div className='d-flex container-fluid flex-column cover my-primary align-items-center'>
-      <div className='d-flex flex-row align-items-center justify-content-evenly'>
-        <div className='d-flex flex-column'>
-          <h1 className='text-size-title jamsil-bold text-center text-white'>
-            내 주변 <span className='bg-light'><span className='text-dark blinkAnimation'>인프라</span></span>를 점수로!</h1>
-          <h1 className='text-size-title jamsil-light text-left text-white'>인프라운드</h1>
+    <div className=" d-flex container-fluid flex-column cover my-primary align-items-center">
+      <div className=" d-flex flex-row align-items-center justify-content-evenly">
+        <div className=" d-flex  flex-column">
+          <h1 className="mobileNone text-size-title jamsil-bold text-center text-white">
+            내 주변{" "}
+            <span className="bg-light">
+              <span className="text-dark blinkAnimation">인프라</span>
+            </span>
+            를 점수로!
+          </h1>
+          <h1 className="mobileNone text-size-title jamsil-light text-left text-white">
+            인프라운드
+          </h1>
         </div>
-        <img src={logoImage}
-            alt="인프라운드 로고"
-            style={{width: 20+'vw'}}/>
+        <img src={logoImage} alt="인프라운드 로고" className="coverLogo" />
       </div>
 
-      <button 
-        id='startButton' 
-        className='btn btn-primary btn-lg my-sub2 border-0 py-2 px-3 shadow-sm'
+      <button
+        id="startButton"
+        className="btn btn-primary btn-lg my-sub2 border-0 py-2 px-3 shadow-sm"
         onClick={handleStartButtonClick}
-        >바로 시작하기</button>
-      <div className='footer'>@2023 All rights reserved.</div>
+      >
+        바로 시작하기
+      </button>
+      <div className="footer">@2023 All rights reserved.</div>
     </div>
-  )
-}
+  );
+};
 
 export default Cover;
--- src/Components/KakaoMap.js
+++ src/Components/KakaoMap.js
@@ -1,11 +1,11 @@
 import { useEffect, useState, useRef } from "react";
 import SideBar from "./SideBar";
-import { Map, MapMarker, ZoomControl } from "react-kakao-maps-sdk";
+import { Circle, CustomOverlayMap, Map, MapMarker, ZoomControl } from "react-kakao-maps-sdk";
 const { kakao } = window;
 const KakaoMap = () => {
-  const [latlng, setLatLng] = useState();
+  const [position, setPosition] = useState();
+  const mapRef = useRef(null);
 
-  const mapRef = useRef < kakao.maps.Map > null;
   const [info, setInfo] = useState("");
 
   const getInfo = () => {
@@ -42,12 +42,92 @@ const KakaoMap = () => {
     setInfo(message);
   };
 
+  // const distancePerLevel = (mapRef, position) => {
+  //   let map = mapRef.current;
+  //   let proj = map.getProjection();
+
+  //   // 지도 중심 좌표를 중심으로 하는 사각형을 구하려고 함
+  //   // center에 지도 중심 말고 원하는 좌표를 넣으면 됨
+  //   let center = position;
+  //   let level = map.getLevel();
+
+  //   // 지도 레벨마다 m값에 해당하는 화면좌표 수치(px)를 구할 수 있으므로
+  //   // 지도 중심의 좌표를 화면좌표로 투영시켜
+  //   // 화면좌표 기준으로 계산할 예정
+  //   let centerPoint = proj.pointFromCoords(center);
+
+  //   // 3레벨에서 1px이 1m
+  //   // 현재 지도 레벨을 기준으로 m당 px값 스케일(px/m)을 구한다.
+  //   let scale = 1 / Math.pow(2, level - 3);
+
+  //   // 구하고자 하는 사각형 한 변의 길이: 25미터
+  //   let len = 25;
+
+  //   // 12.5m에 해당하는 화면좌표(px) 값
+  //   let radius = (len / 2) * scale;
+
+  //   let swPoint = new daum.maps.Point(
+  //     centerPoint.x - pixelForHalfLen,
+  //     centerPoint.y + pixelForHalfLen
+  //   );
+  //   let nwPoint = new daum.maps.Point(
+  //     centerPoint.x + pixelForHalfLen,
+  //     centerPoint.y - pixelForHalfLen
+  //   );
+
+  //   // 화면좌표를 다시 지도의 좌표계 좌표로 변환
+  //   let sw = proj.coordsFromPoint(swPoint);
+  //   let ne = proj.coordsFromPoint(nwPoint);
+
+  //   let rectangleBounds = new daum.maps.LatLngBounds(sw, ne);
+
+  //   let rectangle = new daum.maps.Rectangle({
+  //     map: map,
+  //     bounds: rectangleBounds,
+  //   });
+  // };
+
   return (
-    <Map center={{ lat: 36.815129, lng: 127.1138939 }} style={{ width: "100%", height: "100vh" }}>
-      <MapMarker onClick={getInfo} position={{ lat: 33.55635, lng: 126.795841 }}>
-        <div style={{ color: "#000" }}>Hello World!</div>
+    <Map
+      center={{ lat: 36.815129, lng: 127.1138939 }}
+      style={{ width: "100%", height: "100vh" }}
+      onClick={(_t, mouseEvent) => {
+        setPosition({
+          lat: mouseEvent.latLng.getLat(),
+          lng: mouseEvent.latLng.getLng(),
+        });
+        console.log(position);
+        console.log(mapRef.current.getLevel());
+      }}
+      ref={mapRef}
+    >
+      <MapMarker onClick={getInfo} position={{ lat: 36.85075400414, lng: 127.150372197 }}>
+        <div style={{ color: "#000", display: "flex", alignItems: "center" }}>사무실</div>
       </MapMarker>
       <ZoomControl position={"RIGHT"} />
+      {position && <MapMarker position={position}></MapMarker>}
+      {position && (
+        <CustomOverlayMap zIndex={3} position={position} xAnchor={-0.2} yAnchor={1.6}>
+          <div className="d-flex flex-row bg-body-tertiary shadow-lg px-4 py-3 rounded">
+            <div className="image"></div>
+            <div className="d-flex flex-column">
+              <h4>건물 이름</h4>
+              <p>이곳의 인프라 점수는 8점입니다.</p>
+            </div>
+          </div>
+        </CustomOverlayMap>
+      )}
+      {position && (
+        <CustomOverlayMap className="customOverlay" position={position}>
+          <Circle
+            center={position}
+            radius={200}
+            fillColor={"#FF8080"}
+            fillOpacity={0.4}
+            strokeWeight={0}
+          ></Circle>
+        </CustomOverlayMap>
+      )}
     </Map>
   );
 };
