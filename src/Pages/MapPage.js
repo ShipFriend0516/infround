3900414cf69ac3ff8795637622a8f0b5e7b7b7e5
JeongwooSeo <98446924+ShipFriend0516@users.noreply.github.com>
2023-11-09 18:44:41 +0900

    모바일 업데이트

--- src/Pages/MapPage.js
+++ src/Pages/MapPage.js
@@ -2,11 +2,29 @@ import NavBar from "../Components/NavBar";
 import SideBar from "../Components/SideBar";
 import KakaoMap from "../Components/KakaoMap";
 
+import { useState, useEffect } from "react";
+
 const MapPage = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 500);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 500);
+    };
+
+    // 리사이즈 이벤트에 핸들러 등록
+    window.addEventListener("resize", handleResize);
+
+    // 컴포넌트 언마운트 시 이벤트 리스너 제거
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div>
       <NavBar />
-      <SideBar />
+      <SideBar width={isMobile ? 320 : 480} />
       <KakaoMap />
     </div>
   );
